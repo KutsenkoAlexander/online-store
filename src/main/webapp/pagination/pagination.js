9b4255dfa71d131b1a7c99fe2ea20de4cfcde614
Aleksandr <kucenkoaleksandr@gmail.com>
2016-05-14 16:32:22 +0300

    3.0.0

--- src/main/webapp/pagination/pagination.js
+++ src/main/webapp/pagination/pagination.js
@@ -55,21 +55,4 @@ angular.module('monolitApp.pagination', ['ngResource','monolitApp.sort'])
             }
             return input;
         };
-    })
-
-    .filter('adminPages', function () {
-        return function (input, currentPage, totalPages, range) {
-            currentPage = parseInt(currentPage);
-            totalPages = parseInt(totalPages);
-            range = parseInt(range);
-    
-            var minPage = (currentPage - range < 0) ? 0 : (currentPage - range > (totalPages - (range * 2))) ? totalPages - (range * 2) : currentPage - range;
-            var maxPage = (currentPage + range > totalPages) ? totalPages : (currentPage + range < range * 2) ? range * 2 : currentPage + range;
-    
-            for(var i=minPage; i<maxPage; i++) {
-                input.push(i);
-            }
-    
-            return input;
-        };
     });
