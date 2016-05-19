3fd45a08bb8df251a9ca723a653ff0b1b9a825ee
Aleksandr <kucenkoaleksandr@gmail.com>
2016-05-19 23:41:03 +0300

    v 3.0.0

--- src/main/java/ua/kay/monolit/Application.java
+++ src/main/java/ua/kay/monolit/Application.java
@@ -1,6 +1,5 @@
 package ua.kay.monolit;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
--- src/main/java/ua/kay/monolit/configs/SecurityConfiguration.java
+++ src/main/java/ua/kay/monolit/configs/SecurityConfiguration.java
@@ -1,55 +1,34 @@
 package ua.kay.monolit.configs;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    DataSource dataSource;
-
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
-//            .authorizeRequests()
-//                .antMatchers("/admin",
-//                        "/admin/color",
-//                        "/admin/color/save",
-//                        "/admin/color/delete/*",
-//                        "/admin/goods",
-//                        "/admin/product/*").authenticated()
-//                .and()
-//            .formLogin()
-//                .loginPage("/login")
-//                .permitAll()
-//                .and()
-//            .logout()
-//                .permitAll();
-                .httpBasic()
-            .and()
-                .authorizeRequests()
-                    .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
-                    .anyRequest().authenticated();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .jdbcAuthentication()
-            .dataSource(dataSource)
-                .usersByUsernameQuery("select username,password, enabled from users where username=?")
-                .authoritiesByUsernameQuery("select username, role from user_roles where username=?");
+            .authorizeRequests()
+                .antMatchers("/admin",
+                        "/admin/color",
+                        "/admin/color/save",
+                        "/admin/color/delete/*",
+                        "/admin/goods",
+                        "/admin/product/*").authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
     }
 
 }
