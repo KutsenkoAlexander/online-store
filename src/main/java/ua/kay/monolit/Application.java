1245dd1ed4fb027afa49b8e9c216f24d8c0c02ba
KutsenkoAleksandr <kucenkoaleksandr@gmail.com>
2016-05-25 09:37:18 +0300

    v 3.0.0

--- src/main/java/ua/kay/monolit/Application.java
+++ src/main/java/ua/kay/monolit/Application.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan({"ua.kay.monolit"})
 @EnableAutoConfiguration
-@SpringBootApplication
 public class Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
