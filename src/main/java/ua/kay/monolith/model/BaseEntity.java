e527a370b08114dcd8bd0a46f41452ceb77bbfcf
alexander <kucenkoaleksandr@gmail.com>
2020-03-02 15:34:20 +0200

    Improving code

--- src/main/java/ua/kay/monolith/model/BaseEntity.java
+++ src/main/java/ua/kay/monolith/model/BaseEntity.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 @MappedSuperclass
 public class BaseEntity implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
     private Long id;
 
